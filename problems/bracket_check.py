08ccc126b541b75f3e2eff24a26470ef1e9f810a
nashquant <81002528+nashquant@users.noreply.github.com>
2021-08-20 12:07:07 -0300

    Update bracket_check.py
    Found unsolved cases

--- problems/bracket_check.py
+++ problems/bracket_check.py
@@ -14,9 +14,11 @@ def bracket_validator(string: str) -> bool:
             open_stack.append(char)
         
         elif char in CLOSE:
+            if not open_stack:
+                return False
             if not MATCH[char] == open_stack.pop():
                 return False
-            
+
     return True
             
                 
@@ -24,12 +26,12 @@ if __name__=='__main__':
     test1 = "{ [] ( ) }"
     test2 = "{ [(] ) }"
     test3 = "{ [ }"
+    test4 = "{ { { } } } } }"
+    test5 = "{}{}{}()()[]"
     
     assert bracket_validator(test1)
     assert not bracket_validator(test2)
     assert not bracket_validator(test3)
-    
+    assert not bracket_validator(test4)
+    assert bracket_validator(test5)
             
-    
-    
-        
