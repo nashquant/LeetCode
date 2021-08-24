634a07f48453e096b06dc0e76652e00277981d05
nashquant <81002528+nashquant@users.noreply.github.com>
2021-08-24 00:05:33 -0300

    Create num_divisors.py

---
+++ problems/num_divisors.py
@@ -0,0 +1,28 @@
+def num_divisors(n: int) -> int:
+
+    """
+
+    O(sqrtN)
+    
+    Check: 
+    
+    Factorization: (a^m) * (b^n) * ... * (z ^ k)
+    Therefore: Num_of_divisors = (m+1) * (n+1) * ... * (k+1)
+    
+    """
+
+    divisors = set()
+    
+    i = 1
+    while (i+1)*(i+1) <=n:
+        if n%i == 0:
+            divisors.update([i, int(n/i)])
+        
+        i+=1
+
+    return len(divisors)
+
+if __name__ == '__main__':
+    assert num_divisors(100) == 8
+    assert num_divisors(450) == 18  ### (2) * (3^2) * (5^2) -> divisors = 2 * 3 * 3 = 18
+        
