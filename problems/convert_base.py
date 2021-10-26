87da677d8c4ce91740c5cbfa73f9f7fbe98d9218
nashquant <81002528+nashquant@users.noreply.github.com>
2021-10-26 20:24:39 -0300

    Update convert_base.py

--- problems/convert_base.py
+++ problems/convert_base.py
@@ -1,39 +1,35 @@
-
 """
+- Given a non-negative integer n, convert n to base m (m<=9). 
+- Answer in string format to emphasize the diff to base 10 integer.
+- Do not use any built in base m conversion functions like bin.
 
-Given a non-negative integer n, convert n to base m (m<=9) in string form. Do not use any built in base m conversion functions like bin.
-
-Here's an example and some starter code:
+- Here's an example and some starter code:
 
 def base_m(n, m):
   # Fill this in.
-
 print(base_m(123, 2))
 # 1111011
 
 In the above example, 2^6 + 2^5 + 2^4 + 2^3 + 2^1 + 2^0 = 123.
-
 """
 
 def base_m(x: int, base: int) -> str:
     remainder = x%base
-    divisor = x//base
+    dividend = x//base
 
-    number = [str(remainder)]
+    reversed = [str(remainder)]
 
-    while divisor > base-1:
-        number.append(str(divisor%base))
-        divisor = divisor//base
+    while dividend > base-1:
+        reversed.append(str(dividend%base))
+        dividend = dividend//base
 
-    number.append(str(divisor))
-    strnumber = "".join(number[::-1])
-    return strnumber
-
-
-def check_base(x: int, base: int) -> bool:
-    return x == int(base_m(x, base), base)
+    reversed.append(str(dividend))
+    answer = reversed[::-1]
+    
+    return "".join(answer)
 
 if __name__ == '__main__':
-    assert check_base(16, 2)
-    assert check_base(1023, 2)
-    assert check_base(12381, 5)
+    assert int(base_m(16, 2), 2) == 16
+    assert int(base_m(123, 2), 2) == 123
+    assert int(base_m(1020, 4), 4) == 1020
+    assert int(base_m(3238219, 7), 7) == 3238219
